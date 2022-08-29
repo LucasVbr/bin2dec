3c022469c8701f6c5ed11cc0b06d290712bfe7ab
Lucàs <vabrelucas@gmail.com>
2022-08-29 17:01:23 +0200

    🗑 Remove unused imports

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Box, Center, Flex, Input, Text} from '@chakra-ui/react';
+import {Flex, Input, Text} from '@chakra-ui/react';
 import {ArrowDownIcon} from '@chakra-ui/icons';
 
 export default function Content() {
